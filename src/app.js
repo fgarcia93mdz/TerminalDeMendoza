a51850a9b4d4b18d886b0a3c5356470d6d5a261d
fgarcia93mdz <frangarcia93@icloud.com>
2022-10-08 10:13:39 -0300

    modificaciones peliminares

--- src/app.js
+++ src/app.js
@@ -38,4 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(8080, () => {
+  console.log('El servidor de la terminal de mendoza esta corriendo');
+});
 module.exports = app;
