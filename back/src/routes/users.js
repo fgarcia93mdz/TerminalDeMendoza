ce738a75b918521b20729e6d0bb2166210b1a725
Guille <taboada1991@hotmail.com>
2022-11-04 16:14:11 -0300

    agregado middleware para autorización por roles en el jwt

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const {register, changePassword } = require("../controllers/users");
+const { authenticateToken } = require("../middlewares/authenticateToken.js");
+const verifyRoles = require("../middlewares/verifyRoles");
+const ROLES = require("../config/roles");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register',register)
+//data para el front
+// POST localhost:8080/users/register
+// {
+//   "nombre": "ntest",
+//   "apellido": "atest",
+//   "usuario": "utest",
+//   "password": "ptest",
+//   "rol": "3"
+// }
+
+
+router.post("/changePassword", authenticateToken , verifyRoles(ROLES.Administrador,ROLES.RRHH),changePassword);
+//data para el front
+// POST localhost:8080/users/changePassword
+// {
+//   "password":"ptest",
+//   "nuevaClave":"ptest1"
+// }
+// HEADERS
+// authorization "Bearer token..."
 
 module.exports = router;
