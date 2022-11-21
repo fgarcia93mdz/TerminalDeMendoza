43e8c4d67d05aa3b40f31fdfd990ae3f2126bde9
fgarcia93mdz <frangarcia93@icloud.com>
2022-11-21 20:10:50 -0300

    Configuracion en el config de database para tener en cuenta en el servidor

--- back/src/database/config/config.js
+++ back/src/database/config/config.js
@@ -1,18 +1,20 @@
 module.exports =  {
   "development": {
     "username": "root",
-    "password": "",
-    "database": "terminal_mendoza",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
     "password": "root",
     "database": "terminal_mendoza",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
+  //Usar esta cuando se hace modificaciones en el back para que pegue a la base de datos, hay que sacar el '_servidor'
+  "development_servidor": {
+    "username": "torreter",
+    "password": "003689Fgg$",
+    "database": "torreter_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  
   "production": {
     "username": "root",
     "password": "root",
