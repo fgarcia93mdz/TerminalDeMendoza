7d93549db2f9cd2727fbbe5d5a92b2b1b5d8fa84
nicolasbava <nicobava96@gmail.com>
2022-11-22 22:55:26 -0300

    Added changes Form edit user RRHH

--- front/my-app/src/pages/securityUser/SecurityHome.jsx
+++ front/my-app/src/pages/securityUser/SecurityHome.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material';
 import axios from 'axios'
